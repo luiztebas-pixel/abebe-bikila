1afb00be172754e2f64b411a279e4287480566b7
luiztebas-pixel <luiz.tebas@escola.pr.gov.br>
2025-10-29 08:01:31 -0300

    meus

---
+++ main.js
@@ -0,0 +1,11 @@
+const avanca = document.querySelectorAll(".btn-proximo")
+console.log(avanca)
+
+avanca.forEach(button =>{
+    button.addEventListener("click", function (){ 
+        const atual =document.querySelector("ativo")
+        const proximoPasso = "passo-" +getAttribute("data-proximo")
+        atual.classList.remove("ativo")
+        document.getElementById(proximoPasso). classList.add
+    })
+})
